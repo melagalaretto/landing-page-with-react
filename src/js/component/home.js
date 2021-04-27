176ab76baf866a8aba92c9fe5107fa8b7ef44978
melagalaretto <moonsea1711@gmail.com>
2021-04-27 20:04:01 +0000

    Subida de archivo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -6,19 +6,42 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="#">
+				Start Boostrap
 			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+			<button
+				class="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarNavAltMarkup"
+				aria-controls="navbarNavAltMarkup"
+				aria-expanded="false"
+				aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div
+				class="collapse navbar-collapse justify-content-end"
+				id="navbarNavAltMarkup">
+				<div class="navbar-nav">
+					<a class="nav-link active" href="#">
+						Home <span class="sr-only">(current)</span>
+					</a>
+					<a class="nav-link" href="#">
+						About
+					</a>
+					<a class="nav-link" href="#">
+						Services
+					</a>
+					<a
+						class="nav-link disabled"
+						href="#"
+						tabindex="-1"
+						aria-disabled="true">
+						Contact
+					</a>
+				</div>
+			</div>
+		</nav>
 	);
 }
